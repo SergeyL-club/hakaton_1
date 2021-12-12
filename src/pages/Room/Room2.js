0b8c1c1d92b1234298a5681e7a8ac0b88f3dcf60
Сергей Лапшин <damnazzel@gmail.com>
2021-12-12 16:36:52 +0500

    start and stop button

--- src/pages/Room/Room2.js
+++ src/pages/Room/Room2.js
@@ -125,6 +125,11 @@ const Room = (props) => {
 
     return (
         <div>
+            <button onClick={() => userVideo.current.start()}>Start</button>
+            <button onClick={() => userVideo.current.stop()}>Stop</button>
+            -------------------------------------------------------------
+            <button onClick={() => partnerVideo.current.start()}>Start</button>
+            <button onClick={() => partnerVideo.current.stop()}>Stop</button>
             <video autoPlay ref={userVideo} />
             <video autoPlay ref={partnerVideo} />
         </div>
